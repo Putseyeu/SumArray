6628401a69c296945b93ac25fc781476390908e3
Putseyeu <putseyeu.a.d@gmail.com>
2021-12-26 17:49:37 +0100

    step2

--- Program.cs
+++ Program.cs
@@ -10,45 +10,41 @@ namespace SumArray
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[0];            
-            int sum = 0;            
-            int tempNumber;
-            string userInput = "";
+            int[] numbers = new int[0];
+            string userInput;
             bool exit = false;
             Console.WriteLine($"Введите числа от {int.MinValue} до {int.MaxValue}." +
-                $" Sum - ввывод суммы введеных чисел. Exit - выйти из программы");            
+                $" sum - ввывод суммы введеных чисел. exit - выйти из программы");            
 
-            while (exit == false)
+            while (exit != true)
             {                
                 Console.Write("Введите число или команду: ");
                 userInput = Console.ReadLine();
                 if (userInput == "exit")
-                {                    
-                    break;
+                {
+                    exit = true;
                 }
 
-                if (userInput == "sum")
+                else if (userInput == "sum")
                 {
+                    int sum = 0;
                     for (int i = 0; i < numbers.Length; i++)
                     {
                         sum += numbers[i];
                     }
-                    Console.WriteLine("Сумма равна " + sum);
-                    userInput = "";
-                    sum = 0;
+                    Console.WriteLine("Сумма равна " + sum);                    
                 }
                 else
                 {
                     int intValue;
                     if (int.TryParse(userInput, out intValue))
-                    {
-                        tempNumber = Convert.ToInt32(userInput);                        
+                    {                      
                         int[] tempNumbers = new int[numbers.Length + 1];
                         for (int i = 0; i < numbers.Length; i++)
                         {
                             tempNumbers[i] = numbers[i];
                         }
-                        tempNumbers[tempNumbers.Length - 1] = tempNumber;
+                        tempNumbers[tempNumbers.Length - 1] = intValue;
                         numbers = tempNumbers;                                       
                     }
                     else
